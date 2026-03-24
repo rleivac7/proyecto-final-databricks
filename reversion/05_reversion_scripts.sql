3e2ab5080b18bcab82f0aa3d9568592bea27dee5
rleivac7 <59701883+rleivac7@users.noreply.github.com>
2026-03-24 10:08:04 -0600

    Add files via upload

---
+++ reversion/05_reversion_scripts.sql
@@ -0,0 +1,21 @@
+-- Databricks notebook source
+-- =========================================
+-- Project: Databricks ETL Medallion Architecture
+-- Purpose: Drop logical tables from Bronze, Silver and Gold layers
+-- =========================================
+
+-- GOLD LAYER
+DROP TABLE IF EXISTS ralc_gold.top_products;
+DROP TABLE IF EXISTS ralc_gold.reorders;
+DROP TABLE IF EXISTS ralc_gold.orders_by_day;
+DROP TABLE IF EXISTS ralc_gold.top_departments;
+DROP TABLE IF EXISTS ralc_gold.reorder_rate;
+DROP TABLE IF EXISTS ralc_gold.avg_products_per_order;
+
+-- SILVER LAYER
+DROP TABLE IF EXISTS ralc_silver.sales_detail;
+
+-- BRONZE LAYER
+DROP TABLE IF EXISTS ralc_bronze.orders;
+DROP TABLE IF EXISTS ralc_bronze.products;
+DROP TABLE IF EXISTS ralc_bronze.order_products;
